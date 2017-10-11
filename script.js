a7ca1217f6b458b122e83134a55801758e0aebab
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-11 17:09:06 +0300

    add pick

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-// ///////////////////////////
-// v1
-// ///////////////////////////
-console.info("v1.1");
-var a = [2,1,5,0,3,4,7,2,3,1,0], //10
-	b = [2, 5, 1, 3, 1, 2, 1, 7, 7, 6], // 17
-	c = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0], // 10
-	d = [7, 0, 1, 3, 4, 1, 2, 1], // 9
-	e = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0], // 10
-	f = [2, 2, 1, 2, 2, 3, 0, 1, 2] ,// 4
-	j = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8], // 24
-	k = [2, 2, 2, 2, 2] ;// 0
-
-
 function res(data){ 
 
-	var sum = null;
+	var sum = null,  
+  	pic = null,
+    picArr = [],
+    arrWater = [];
+    
 
-    diffArr=[];
-    for(var i=0; i<data.length-1; i++){
-        diffArr.push(data[i+1]-data[i]);
- 			sum = sum + data[i];   
+    for(var i = 0; i < data.length; i++){
+      if(pic < data[i]){
+      	pic = data[i];
+        picArr.push(i);
+      }
+      
+      if(data[i] > data[i+1]){
+      	 wsum = data[i] - data[i+1] ;
+ 				 arrWater.push(wsum);
+      }
+
     }
+    	var sum =  Object.values(arrWater).reduce((a, b) => a + b);
+     console.log("picArr",picArr);
+    console.log("arrWater",arrWater);
+
     return sum;
 }
 
 
 
-console.log(res(a) , "result must be 10");
-console.log(res(b), "result must be 17");
-console.log(res(c), "result must be 10");
-console.log(res(d), "result must be 9");
-console.log(res(e), "result must be 10");
-console.log(res(f), "result must be 4");
-console.log(res(j), "result must be 24");
-console.log(res(k), "result must be 0");
+console.log("result is: ", res(a) , "a - result must be 10");
+console.log("result is: ", res(b), "b - result must be 17");
+console.log("result is: ", res(c), "c - result must be 10");
+console.log("result is: ", res(d), "d - result must be 9");
+console.log("result is: ", res(e), "e - result must be 10");
+console.log("result is: ", res(f), "f - result must be 4");
+console.log("result is: ", res(j), "j - result must be 24");
+console.log("result is: ", res(k), "k - result must be 0");
