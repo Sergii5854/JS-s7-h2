3095c0b5a2a92332eba6a54c3b940a3a89d08aa9
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-20 11:36:28 +0300

    fix varibls names

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // ///////////////////////////
 // v1
 // ///////////////////////////
-console.info("v1.2");
+ console.info("v1.6");
+
 var a = [2,1,5,0,3,4,7,2,3,1,0] , //10
 	b = [2, 5, 1, 3, 1, 2, 1, 7, 7, 6] , // 17
 	c = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0] , // 10
@@ -12,7 +13,7 @@ var a = [2,1,5,0,3,4,7,2,3,1,0] , //10
 	k = [2, 2, 2, 2, 2] ;// 0
 
 var h = [7, 0, 1, 3, 4, 1, 2, 1] , // 9 
-	i = [10, 0, 5, 5, 5, 0, 1, 5, 0, 0] ,// 14 
+	i_arr = [10, 0, 5, 5, 5, 0, 1, 5, 0, 0] ,// 14 
 	l = [100, 0, 5, 6, 8, 0] ;// 13 
 
 function res( data ){ 
@@ -20,7 +21,7 @@ function res( data ){
   var sum = 0,
   between = null,
   left = null,
-  right = data[ data.length - 1 ];
+  right = data[data.length - 1];
 
   data.forEach( function  ( elem, index ){
 
@@ -35,13 +36,13 @@ function res( data ){
 
   for ( i = data.length - 1; i > between; i-- ) {
 
-    if( data[ i ] > right) {
-      right = data[ i ];      
+    if( data[i] > right) {
+      right = data[i];      
     } else {
     	null
     }
 
-  	sum = sum - ( data[ between ] - right ); 
+  	sum = sum - ( data[between] - right ); 
 
   }// end for 
 
@@ -50,8 +51,8 @@ function res( data ){
 };// end res function 
 
 
-console.log("result is: ", res(a) , "a - result must be 10");
-console.log("result is: ", res(b), "b - result must be 17");
+console.log("result is: ", res( a ) , "a - result must be 10");
+console.log("result is: ", res( b ), "b - result must be 17");
 console.log("result is: ", res( c ), "c - result must be 10");
 console.log("result is: ", res( d ), "d - result must be 9");
 console.log("result is: ", res( e ), "e - result must be 10");
@@ -59,13 +60,9 @@ console.log("result is: ", res( f ), "f - result must be 4");
 console.log("result is: ", res( j ), "j - result must be 24");
 console.log("result is: ", res( k ), "k - result must be 0"); 
 
-console.log("result is: ", res( [7, 0, 1, 3, 4, 1, 2, 1] ), "- result must be 9"); 
-console.log("result is: ", res( [10, 0, 5, 5, 5, 0, 1, 5, 0, 0] ), "- result must be 14"); 
-console.log("result is: ", res( [100, 0, 5, 6, 8, 0]), " - result must be 13"); 
 
-console.log("result is: ", res( h ), "- result must be 9"); 
-console.log("result is: ", res( i ), "- result must be 14"); // why brouser show error in this array ? 
-console.log("result is: ", res( l ), " - result must be 13"); 
+console.log("result is: ", res( h ), " h - result must be 9"); 
+console.log("result is: ", res( i_arr ), " i - result must be 14"); 
+console.log("result is: ", res( l ), " l - result must be 13"); 
 
-
-
+ 
